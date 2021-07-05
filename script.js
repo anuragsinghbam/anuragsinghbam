0f5c233a5ad009c7d769a80b2dfa33def9f6ccc5
Anurag Singh <anuragsinghbam@gmail.com>
2021-07-05 05:33:46 +0530

    make whole site responsive

--- script.js
+++ script.js
@@ -53,8 +53,13 @@ latestWorksArrow.addEventListener('click', (e) => {
 // })
 
 var swiper = new Swiper(".mySwiper", {
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
